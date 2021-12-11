9b921beb472ba490c802365a93da3d4948fe65d4
Rjkzavr <kolya.toropof@gmail.com>
2021-12-11 14:59:12 +0300

    make castled (work if on shah)
    castled work if not pieces is not moved
    but work if figure on shah

--- core/pieces/King.py
+++ core/pieces/King.py
@@ -1,11 +1,20 @@
 from core.pieces.Pieces import Pieces
+from core.pieces.Rook import Rook
 
 
 class King(Pieces):
     def __init__(self, game, cell, surface, color):
         super().__init__(game, cell, surface, color)
+        self.can_castled = False
 
     def check_move(self, pos):
         if abs(self.cell[0] - pos[0]) < 2 and abs(self.cell[1] - pos[1]) < 2:
-            return self.check_not_friendly_cell(pos)
+            if self.check_not_friendly_cell(pos):
+                self.can_castled = True
+                return True
+        figure = self.game.board.get_pos(pos)
+        if not self.can_castled and type(figure) is Rook and not figure.is_moved and figure.color == self.color:
+            self.can_castled = True
+            figure.set_cell(self.cell[:])
+            return True
         return False
--- core/pieces/Rook.py
+++ core/pieces/Rook.py
@@ -4,6 +4,7 @@ from core.pieces.Pieces import Pieces
 class Rook(Pieces):
     def __init__(self, game, cell, surface, color):
         super().__init__(game, cell, surface, color)
+        self.is_moved = False
 
     def check_move(self, pos):
         move_x, move_y = pos[0] - self.cell[0], pos[1] - self.cell[1]
@@ -11,6 +12,8 @@ class Rook(Pieces):
             move_x = 1 if move_x > 0 else (-1 if move_x < 0 else 0)
             move_y = 1 if move_y > 0 else (-1 if move_y < 0 else 0)
             # print([self.check_clear_cell((self.cell[0] + move_x * i, self.cell[1] + move_y * i)) for i in range(1, abs(self.cell[0] - pos[0] + self.cell[1] - pos[1]))])
-            return all([self.check_clear_cell((self.cell[0] + move_x * i, self.cell[1] + move_y * i)) for i in range(1, abs(self.cell[0] - pos[0] + self.cell[1] - pos[1]))]) and self.check_not_friendly_cell(pos)
+            if all([self.check_clear_cell((self.cell[0] + move_x * i, self.cell[1] + move_y * i)) for i in range(1, abs(self.cell[0] - pos[0] + self.cell[1] - pos[1]))]) and self.check_not_friendly_cell(pos):
+                self.is_moved = True
+                return True
         return False
 
