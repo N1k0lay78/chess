16c3c1d1015be4275d2b525e39cd817a07422292
Rjkzavr <49759787+N1k0lay78@users.noreply.github.com>
2021-12-11 16:23:40 +0300

    Delete tmp2.py

--- tmp2.py
+++
@@ -1,7 +0,0 @@
-from time import time, sleep
-
-count = 10**4
-rotate = ['—', '—', '—', '\\', '\\', '|', '|', '/', '/']
-for i in range(count):
-    print(f'\r{round((i+1)/count * 100, 2)}% \t{rotate[int(time()*4) % len(rotate)]} \tfile №{i}', end="")
-    sleep(0.005)
