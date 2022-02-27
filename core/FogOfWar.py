94fbf4a45cfb8c5aa071f68d12bb4c2bf5dbb479
Rjkzavr <kolya.toropof@gmail.com>
2022-02-27 11:15:41 +0300

    refactor FogOfWar update

--- core/FogOfWar.py
+++ core/FogOfWar.py
@@ -24,48 +24,26 @@ class FogOfWar:
     def update(self):
         self.map = [[1] * (8 + self.padding * 2) for _ in range(8 + self.padding * 2)]
         for figure in self.game.board.board:
-            if figure.color == self.game.color:
+            if figure.color == self.game.board.color:
                 if type(figure) == Pawn:
-                    self.map[figure.cell[1] + self.padding - 1][figure.cell[0] + self.padding] = 0
-                    self.map[figure.cell[1] + self.padding][figure.cell[0] + self.padding] = 0
-                    self.map[figure.cell[1] + self.padding - 1][figure.cell[0] + self.padding - 1] = 0
-                    self.map[figure.cell[1] + self.padding][figure.cell[0] + self.padding - 1] = 0
-                    self.map[figure.cell[1] + self.padding - 1][figure.cell[0] + self.padding + 1] = 0
-                    self.map[figure.cell[1] + self.padding][figure.cell[0] + self.padding + 1] = 0
+                    for y in range(-1, 1):  # [-1;0]
+                        for x in range(-1, 2):  # [-1;1]
+                            self.map[figure.cell[1] + self.padding + y][figure.cell[0] + self.padding + x] = 0
                 if type(figure) in [Horse, King, Queen]:
-                    self.map[figure.cell[1] + self.padding + 1][figure.cell[0] + self.padding - 2] = 0
-                    self.map[figure.cell[1] + self.padding + 1][figure.cell[0] + self.padding - 1] = 0
-                    self.map[figure.cell[1] + self.padding + 1][figure.cell[0] + self.padding] = 0
-                    self.map[figure.cell[1] + self.padding + 1][figure.cell[0] + self.padding + 1] = 0
-                    self.map[figure.cell[1] + self.padding + 1][figure.cell[0] + self.padding + 2] = 0
-                    self.map[figure.cell[1] + self.padding][figure.cell[0] + self.padding - 2] = 0
-                    self.map[figure.cell[1] + self.padding][figure.cell[0] + self.padding - 1] = 0
-                    self.map[figure.cell[1] + self.padding][figure.cell[0] + self.padding] = 0
-                    self.map[figure.cell[1] + self.padding][figure.cell[0] + self.padding + 1] = 0
-                    self.map[figure.cell[1] + self.padding][figure.cell[0] + self.padding + 2] = 0
-                    self.map[figure.cell[1] + self.padding - 1][figure.cell[0] + self.padding - 2] = 0
-                    self.map[figure.cell[1] + self.padding - 1][figure.cell[0] + self.padding - 1] = 0
-                    self.map[figure.cell[1] + self.padding - 1][figure.cell[0] + self.padding] = 0
-                    self.map[figure.cell[1] + self.padding - 1][figure.cell[0] + self.padding + 1] = 0
-                    self.map[figure.cell[1] + self.padding - 1][figure.cell[0] + self.padding + 2] = 0
+                    for y in range(-1, 2):  # [-1;-1]
+                        for x in range(-2, 3):  # [-2;2]
+                            self.map[figure.cell[1] + self.padding + y][figure.cell[0] + self.padding + x] = 0
                 else:
-                    self.map[figure.cell[1] + self.padding + 1][figure.cell[0] + self.padding] = 0
-                    self.map[figure.cell[1] + self.padding + 1][figure.cell[0] + self.padding - 1] = 0
-                    self.map[figure.cell[1] + self.padding + 1][figure.cell[0] + self.padding + 1] = 0
-                    self.map[figure.cell[1] + self.padding][figure.cell[0] + self.padding] = 0
-                    self.map[figure.cell[1] + self.padding][figure.cell[0] + self.padding - 1] = 0
-                    self.map[figure.cell[1] + self.padding][figure.cell[0] + self.padding + 1] = 0
-                    self.map[figure.cell[1] + self.padding - 1][figure.cell[0] + self.padding] = 0
-                    self.map[figure.cell[1] + self.padding - 1][figure.cell[0] + self.padding - 1] = 0
-                    self.map[figure.cell[1] + self.padding - 1][figure.cell[0] + self.padding + 1] = 0
+                    for y in range(-1, 2):  # [-1;1]
+                        for x in range(-1, 2):  # [-1;1]
+                            self.map[figure.cell[1] + self.padding + y][figure.cell[0] + self.padding + x] = 0
 
     def draw(self):
         for x in range(8 + self.padding * 2):
             for y in range(8 + self.padding * 2):
-                # print(*self.map, sep='\n')
                 if self.map[y][x]:
                     self.game.screen.blit(self.tile_set[fog['full'][0]], (x * self.size[0] + self.pos[0], y * self.size[1] + self.pos[1]))
-                elif 1 <= x < self.padding * 2 + 7 and 1 <= y < self.padding * 2 + 7:
+                elif 1 <= x < self.padding * 2 + 7 and 1 <= y < self.padding * 2 + 7:  # borders
                     if self.map[y + 1][x] and not self.map[y - 1][x] and self.map[y][x + 1] and not self.map[y][x - 1]:
                         self.game.screen.blit(self.tile_set[fog['bottom-right'][0]], (x * self.size[0] + self.pos[0], y * self.size[1] + self.pos[1]))
                     elif self.map[y + 1][x] and not self.map[y - 1][x] and not self.map[y][x + 1] and self.map[y][x - 1]:
