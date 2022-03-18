63468c6012e9cf33ccca977c8c74dcc4a5905a8f
Rjkzavr <kolya.toropof@gmail.com>
2022-03-18 18:37:31 +0300

    fix movement of the horse

--- core/logic/pieces_move.py
+++ core/logic/pieces_move.py
@@ -15,7 +15,9 @@ def horse_move(self, pos):
     # clockwise from 12 o'clock
     moves = ((2, 1), (1, 2), (-1, 2), (-2, 1), (-2, -1), (-1, -2), (1, -2), (2, -1))
     # heck that the target is where we can move
-    return any([True for move in moves if pos == (self.cell[0] + move[0], self.cell[1] + move[1]) and self.check_not_friendly_cell(pos)])
+    return any([True for move in moves
+                if pos[0] == self.cell[0] + move[0] and pos[1] == self.cell[1] + move[1]
+                and self.check_not_friendly_cell(pos)])
 
 
 def king_move(self, pos):
