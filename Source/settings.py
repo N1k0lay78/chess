c2b826f86ac2e9667480b18d64b41ee1b18b4194
Rjkzavr <kolya.toropof@gmail.com>
2022-04-01 23:20:20 +0300

    make function for generating random nickname

--- Source/settings.py
+++ Source/settings.py
@@ -1,3 +1,5 @@
+import random
+
 params = {
     "mode": "offline",  # online/offline/fog of war
     "board_name": "test2",
@@ -10,25 +12,24 @@ params = {
     "app_name": "Chess Fog of War",
     "app_icon": "Source/Image/icon.png",
     "max_fps": 30,
-    "start_window": "Game",  # "Load" for production
+    "is_on_rotation": True,
+    "start_window": "Menu",  # "Load" for production
     "is_flip_screen": False,
 }
 
-is_on_fog_of_war = False
-name_board_to_play = "test2"
-is_online = True
-# debug = False
-# online_host_ip = "192.168.1.71"  # "192.168.1.13" "26.238.243.152"
-# online_host_port = 8080
-debug = True
-online_host_ip = "192.168.1.13"  # "26.238.243.152"
-online_host_port = 5050
-nickname = ""  # if empty use input
+# is_on_fog_of_war = False
+# name_board_to_play = "test2"
+# is_online = True
+# # debug = False
+# # online_host_ip = "192.168.1.71"  # "192.168.1.13" "26.238.243.152"
+# # online_host_port = 8080
+# debug = True
+# online_host_ip = "192.168.1.13"  # "26.238.243.152"
+# online_host_port = 5050
+# nickname = ""  # if empty use input
+
+nicknames = ["NIKI", "RJKZ", "RJKZAVR", "NIKOLAUSUS", "NIKNIKSHAM", "TSAR", "N1K0LAY78", "NIKTV_78"]
 
 
-def set_param(param, value):
-    params[param] = value
-
-
-def get_param(param):
-    return params[param]
+def set_random_nickname():
+    params["nickname"] = random.choice(nicknames)
